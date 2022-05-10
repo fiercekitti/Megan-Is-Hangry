4ffb03278a0964f8254cb4a0aca374fc6f562635
Megan Batchelor <meganlennek@gmail.com>
2022-05-10 10:28:12 -0500

    Search Bar Completion

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+    return (
+      <View style={styles.backgroundStyle}>
+        <Ionicons name="ios-search-outline" style={styles.iconStyle}/>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.inputStyle}
+          placeholder="Search"
+          value={term}
+          onChangeText={onTermChange}
+          onEndEditing={onTermSubmit}
+        />
+      </View>
+    );
+  };
+
+
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor:'#f0eeee',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection:'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 40,
+        alignSelf: 'center',
+        marginHorizontal: 10
+    }
+});
+
+export default SearchBar;
