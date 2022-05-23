4bc6dfcf4ead824337181b942f7a8529999c0264
Megan Batchelor <meganlennek@gmail.com>
2022-05-23 09:33:39 -0500

    Update for React Native and OutsideAPI calls

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
+    //search term 
     const [term, setTerm] = useState('');
+    //yelp api call results
+    const [results, setResults] = useState([]);
+    //error message for user
+    const [errorMessage, setErrorMessage] = useState('');
     
+    const searchAPI = async (searchTerm) => {
+        console.log('Hi there!')
+        try {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'lenexa'
+            }
+        });
+        setResults(response.data.businesses);
+    } catch (err) {
+        setErrorMessage('Something went wrong. Please try again.')
+    }
+
+    };
+   
+    //Call searchAPI when compenent is first rendered
+    //searchAPI('pasta')
+
     return (
     <View>
         <SearchBar 
             term={term} 
-            onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitted')} 
+            onTermChange={setTerm}
+            onTermSubmit={() => searchAPI(term)} 
         />
-        <Text>Search Screen</Text>
-        <Text>{term}</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text>We have found {results.length} results </Text>
     </View>
     );
 };
