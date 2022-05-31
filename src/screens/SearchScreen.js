ce94df6a316412c2b27626254ac61647685ba8b2
Megan Batchelor <meganlennek@gmail.com>
2022-05-31 15:11:58 -0500

    useEffect complete

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,47 +1,36 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/resultsList';
 
 const SearchScreen = () => {
-    //search term 
-    const [term, setTerm] = useState('');
-    //yelp api call results
-    const [results, setResults] = useState([]);
-    //error message for user
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const searchAPI = async (searchTerm) => {
-        console.log('Hi there!')
-        try {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: searchTerm,
-                location: 'lenexa'
-            }
-        });
-        setResults(response.data.businesses);
-    } catch (err) {
-        setErrorMessage('Something went wrong. Please try again.')
-    }
+  //call to Yelp API
+  const [term, setTerm] = useState('');
+  //Hooks for business search
+  const [searchApi, results, errorMessage] = useResults();
 
-    };
-   
-    //Call searchAPI when compenent is first rendered
-    //searchAPI('pasta')
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(results => {
+        return results.price === price;
+    });
+  };
 
-    return (
+  return (
     <View>
-        <SearchBar 
-            term={term} 
-            onTermChange={setTerm}
-            onTermSubmit={() => searchAPI(term)} 
-        />
-        {errorMessage ? <Text>{errorMessage}</Text> : null}
-        <Text>We have found {results.length} results </Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} header="Cheap Cheap" />
+      <ResultsList results={filterResultsByPrice('$$')} header="Average"/>
+      <ResultsList results={filterResultsByPrice('$$$')} header="Expensive"/>
     </View>
-    );
+  );
 };
 
 const styles = StyleSheet.create({});
